141ac117e05c74cbd0affabe5f0c0a1735190992
Alex Carvalho <95773560+alex14022004@users.noreply.github.com>
2022-09-30 15:33:40 -0300

    Adicionando Struct Enemy e colisao em monstros.h

--- monstros.h
+++ monstros.h
@@ -1,1 +1,20 @@
+#ifndef ENEMIES
+#define ENEMIES
 
+typedef struct{
+  bool active;
+  Rectangle FrameRec;
+  Rectangle Collision;
+  Texture2D Img;
+  Vector2 Position;
+  int MaxFrame;
+  int CurrentFrame;
+  float Width;
+  float Height;
+  float Temp;
+  float FrameSpeed;
+} Enemy;
+
+float enemyColisao(Rectangle enemy, Rectangle player);
+
+#endif ENEMIES
