983e7d6df35035a56fb7c69f8a742334074a1ea5
Alex Carvalho <95773560+alex14022004@users.noreply.github.com>
2022-09-30 15:38:50 -0300

    Adicionando Funcao colisao no monstros.c

--- monstro.c
+++ monstro.c
@@ -1,1 +1,7 @@
+#include<stdio.h>
+#include "monstros.h"
 
+float enemyColisao(Rectangle enemy, Rectangle player){
+    float collision = CheckCollisionRecs(enemy, player);
+    return collision;
+}
