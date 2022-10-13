984972b6c293818dd496254fbe9ab18a4a195c23
xupenio04 <106712324+xupenio04@users.noreply.github.com>
2022-10-13 16:07:21 -0300

    Update Boss.h

--- Boss.h
+++ Boss.h
@@ -1,1 +1,21 @@
 
+#ifndef BOSS
+#define BOSS
+#include "raylib.h" 
+
+typedef struct{
+  Rectangle FrameRec;
+  Rectangle Collision;
+  Texture2D Img;
+  Vector2 Position;
+  int MaxFrame;
+  int CurrentFrame;
+  float Width;
+  float Height;
+  float Temp;
+  float FrameSpeed;
+  float Vida;
+  int CurrentFrameAtaq;
+} Boss;
+
+#endif
