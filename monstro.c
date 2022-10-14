6fabc9b597ad44b21e23ffc84821eec5e3e1bb52
anaalbsantos <114191342+anaalbsantos@users.noreply.github.com>
2022-10-13 22:22:13 -0300

    Delete monstro.c

--- monstro.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include "monstros.h"
-#include "raylib.h"
-
-float enemyColisao(Rectangle enemy, Rectangle player){
-    float collision = CheckCollisionRecs(enemy, player);
-    return collision;
-}
