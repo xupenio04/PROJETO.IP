e37e06962893e409a5c3b295ae61f66c451122f9
anaalbsantos <114191342+anaalbsantos@users.noreply.github.com>
2022-10-14 21:01:52 -0300

    Update enemies.h

--- enemies.h
+++ enemies.h
@@ -2,7 +2,6 @@
 #define ENEMIES_H
 
 #include "raylib.h"
-//#include "player.h"
 
 typedef struct{
   Rectangle FrameRec;
@@ -34,54 +33,31 @@ typedef struct{
   int CurrentFrameAtaq;
 } Boss;
 
-Boss boss;
-Enemy enemie;
-Enemy monstro;
+extern Boss boss;
+extern Enemy enemie;
+extern Enemy monstro;
+extern Enemy voador;
 
-int estadoBoss = 1; //se o boss n tiver atacando
-int estadoEnemy = 3; //se o enemy tiver atacando
-int estadoMonstro = 1;
+extern int estadoBoss; //se o boss n tiver atacando
+extern int estadoEnemy; //se o enemy tiver atacando
+extern int estadoMonstro;
+extern int estadoVoador;
 
-Color ColorMonstro = RAYWHITE;
-Color Colorboss = RAYWHITE;
-Color ColorEnemy = RAYWHITE;
+extern bool monstroMorreu;
+extern bool enemieMorreu;
+extern bool bossMorreu;
+extern bool voadorMorreu;
 
-void iniciarBoss(){
-  boss.Vida = 50.0;
-    boss.Img = (Texture2D) LoadTexture("assets/boss.png");
-    boss.Width = (float) boss.Img.width/7;
-    boss.Height = 163.0;
-    boss.MaxFrame = 5;
-    boss.CurrentFrame = 0;
-    boss.CurrentFrameAtaq = 0;
-    boss.Position = (Vector2){ 500.0, 200.0};//boss.Position = (Vector2) {screenWidth/2-100,screenHeight/2-100};
-    boss.Temp = 0;
-    boss.FrameSpeed = 0.1;
-}
-void iniciarEnemie(){
-  enemie.Vida = 50;
-    enemie.Img = (Texture2D) LoadTexture("assets/enemy.png");
-    enemie.Width = (float) enemie.Img.width/4;
-    enemie.Height = (float) enemie.Img.height/6;
-    enemie.MaxFrame = 5;
-    enemie.CurrentFrame = 0;
-    enemie.Position = (Vector2) {500.0, 300.0};
-    enemie.Temp = 0;
-    enemie.CurrentFrameAtaq = 0;
-    enemie.FrameSpeed = 0.1;
-}
-void iniciarMonstro(){
-  // essa funcao carrega a textura
-    monstro.Img = (Texture2D) LoadTexture("assets/monstroOculos.png");
-    monstro.Width = (float) monstro.Img.width/4;
-    monstro.Height = (float) monstro.Img.height/4;
-    monstro.MaxFrame = 5;
-    monstro.CurrentFrame = 0;
-    monstro.CurrentFrameAtaq = 0;
-    monstro.Position = (Vector2) {400.0, 400.0};
-    monstro.Temp = 0;
-    monstro.FrameSpeed = 0.1;
-    monstro.Vida = 50;
-}
+
+extern Color ColorMonstro;
+extern Color Colorboss;
+extern Color ColorEnemy;
+extern Color ColorVoador;
+ 
+
+void iniciarBoss();
+void iniciarEnemie();
+void iniciarMonstro();
+void iniciarVoador();
 
 #endif
